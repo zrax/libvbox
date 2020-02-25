c65fe8891afa15d6c4411dc98406595174c2dff5
Michael Hansen <zrax0111@gmail.com>
2020-02-25 10:42:07 -0800

    Support new SDK versions.  No API changes in new SDKs.

--- lib/vboxwrap.cpp
+++ lib/vboxwrap.cpp
@@ -34,11 +34,11 @@ static bool isApiCompatible(int sdkVersion, int apiVersion)
         { VBox_MAKE_VERSION(5, 1, 36), VBox_MAKE_VERSION(5, 1, 38) },
 
         { VBox_MAKE_VERSION(5, 2, 0), VBox_MAKE_VERSION(5, 2, 8) },
-        { VBox_MAKE_VERSION(5, 2, 10), VBox_MAKE_VERSION(5, 2, 36) },
+        { VBox_MAKE_VERSION(5, 2, 10), VBox_MAKE_VERSION(5, 2, 38) },
 
-        { VBox_MAKE_VERSION(6, 0, 0), VBox_MAKE_VERSION(6, 0, 16) },
+        { VBox_MAKE_VERSION(6, 0, 0), VBox_MAKE_VERSION(6, 0, 18) },
 
-        { VBox_MAKE_VERSION(6, 1, 0), VBox_MAKE_VERSION(6, 1, 2) },
+        { VBox_MAKE_VERSION(6, 1, 0), VBox_MAKE_VERSION(6, 1, 4) },
     };
 
     if (sdkVersion > apiVersion)
