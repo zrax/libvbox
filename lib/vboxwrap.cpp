f505506759b498a22a8416f21186b7840687bbb4
Michael Hansen <zrax0111@gmail.com>
2021-04-30 09:16:09 -0700

    Update supported SDK versions for 6.1.22.  No API changes.

--- lib/vboxwrap.cpp
+++ lib/vboxwrap.cpp
@@ -39,7 +39,7 @@ static bool isApiCompatible(int sdkVersion, int apiVersion)
         { VBox_MAKE_VERSION(6, 0, 0), VBox_MAKE_VERSION(6, 0, 24) },
 
         { VBox_MAKE_VERSION(6, 1, 0), VBox_MAKE_VERSION(6, 1, 18) },
-        { VBox_MAKE_VERSION(6, 1, 20), VBox_MAKE_VERSION(6, 1, 20) },
+        { VBox_MAKE_VERSION(6, 1, 20), VBox_MAKE_VERSION(6, 1, 22) },
     };
 
     if (sdkVersion > apiVersion)
