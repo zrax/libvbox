f6de45557e373fbadac47e7473a0ea25ffe87866
Michael Hansen <zrax0111@gmail.com>
2025-08-14 13:58:51 -0700

    Support VBox SDK 7.1.12.  No API changes.

--- lib/vboxwrap.cpp
+++ lib/vboxwrap.cpp
@@ -41,7 +41,7 @@ static bool isApiCompatible(int sdkVersion, int apiVersion)
         { VBox_MAKE_VERSION(6, 1, 0), VBox_MAKE_VERSION(6, 1, 50) },
 
         { VBox_MAKE_VERSION(7, 0, 0), VBox_MAKE_VERSION(7, 0, 26) },
-        { VBox_MAKE_VERSION(7, 1, 0), VBox_MAKE_VERSION(7, 1, 8) },
+        { VBox_MAKE_VERSION(7, 1, 0), VBox_MAKE_VERSION(7, 1, 12) },
     };
 
     if (sdkVersion > apiVersion)
